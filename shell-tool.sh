7bcbc7a84003d7449c2cec6673103be366809c51
liy812118@163.com <ljy812118@163.com>
2019-06-23 21:58:17 +0800

    init

--- shell-tool.sh
+++ shell-tool.sh
@@ -1,5 +1,6 @@
 branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-# cp -r ../前端知识总结 ./
+cp -r ../前段知识总结 ./
 git add .
-git commit -am init
+read -p '请输入您的commit信息' commitInfo
+git commit -am ${commitInfo}
 git push origin ${branch}
